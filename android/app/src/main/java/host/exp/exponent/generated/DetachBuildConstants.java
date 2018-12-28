e7b8b5ba4c66b09541e16d5fa6809421d5b17b1f
vishal <vishal@webklabs.com>
2018-12-28 19:37:02 +0530

    new code

--- android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++
@@ -1,8 +0,0 @@
-package host.exp.exponent.generated;
-
-// This file is auto-generated. Please don't rename!
-public class DetachBuildConstants {
-
-  public static final String DEVELOPMENT_URL = "expb2bdde924b924d8994e5aac008ed0a2e://192.168.1.12:19000";
-
-}
--- android/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./gradlew ${1:-installDevMinSdkDevKernelDebug} --stacktrace && adb shell am start -n mjzenith.com.mjzenith/host.exp.exponent.MainActivity
